64c3856840f7faf5ba6427e7425d85e9f7fe2132
Navdeep Sharma <navdeepisno1@gmail.com>
2022-07-14 22:03:04 +0530

    Update Mr.Kim and Customers.cpp

--- samsung/SWC/algorithm/Mr.Kim and Customers.cpp
+++ samsung/SWC/algorithm/Mr.Kim and Customers.cpp
@@ -1,16 +1,40 @@
+//Author NAVDEEPISNO1
+//Navdeep Sharma
+
+
+
+/*
+ALgorithm
+
+The basic idea used here is we will enumerate every possible combination
+
+Steps:
+1 As we know that we have two coordinates everywhere and we need them at same time to perform calculation given in the question (Here Distance).
+2 Create struct to hold both x & y .Here we call it as Point
+3 Always create Functions in bottom up manner. i.e main should be bottommost , helper function must be above main, any other utility function in the same way as previous one.
+4 Inorder to track Visited Customer create an boolean 1D array of Size MAX_CLIENTS .i.e 10 here
+
+*/
+
+
+//Include basic header files
 #include <bits/stdc++.h>
+
+//Add namespace to remove confusion among variables
 using namespace std;
 
+//Create stucture to hold the coordinate or point .Its best practice to create it above all the variables to avoid compiler error
 struct Point
 {
     int x;
     int y;
 };
 
+//Create a global visited array & Customers array
 bool visited[10];
 Point pointCustomers[10];
 
-//State can be true or false
+//Utility function to set State of Visited Array.(State can be true or false)
 void setVisitedState(bool state)
 {
     for(int i=0;i<10;i++)
@@ -20,21 +44,22 @@ void setVisitedState(bool state)
 }
 
 //All Helper Functions at one place above the main Helper function i.e solveUtility()
-
+//Self Defined
 int getAbsValueUtil(int v1,int v2)
 {
     return v1>v2 ? v1-v2 : v2-v1;
 }
 
+//Self Defined
 int getDistanceUtil(Point pSource,Point pDestination)
 {
     return getAbsValueUtil(pSource.x,pDestination.x) + getAbsValueUtil(pSource.y,pDestination.y);
 }
 
-
+//Self Defined
 int solveUtility(int parcelDelivered,int nCustomers,Point pHome,Point pCurrent)
 {
-    //Base condition we delivered all parcels so return the distance with home otherwise office
+    //Base condition we delivered all parcels so return the distance with home.
     if(parcelDelivered==nCustomers)
     {
         return getDistanceUtil(pCurrent,pHome);
@@ -71,9 +96,8 @@ int solveUtility(int parcelDelivered,int nCustomers,Point pHome,Point pCurrent)
 }
 
 
-
-int main() {
-	
+//Main function from where CPU start reading the program
+int main() {	
 	//Get number of testcases
 	int t;
 	cin>>t;
@@ -85,20 +109,24 @@ int main() {
 	    int nCustomers;
 	    
 	    //Store x and y in variables
-        int x,y;
+            int x,y;
 	    
+		//Get the number of customers
 	    cin>>nCustomers;
 	    
+		//Get Home coordinates or points
 	    Point pHome;
 	    cin>>x>>y;
 	    pHome.x = x;
 	    pHome.y = y;
 	    
+		//Get Office coordinates or points
 	    Point pOffice;
 	    cin>>x>>y;
 	    pOffice.x = x;
 	    pOffice.y = y;
 	    
+		//Get Customer coordinates or points and fill in the array
 	    for(int i=0;i<nCustomers;i++)
 	    {
 	        Point p;
@@ -109,10 +137,14 @@ int main() {
 	        pointCustomers[i] = p;
 	    }
 	    
+		//Set Visited Array to false
 	    setVisitedState(false);
+		
+		//Set number of parcel delivered or number of customers reached
 	    int parcelDelivered = 0;
-	    cout<<solveUtility(parcelDelivered,nCustomers,pHome,pOffice)<<endl;
-	    
+		
+		//Output the result
+	    cout<<solveUtility(parcelDelivered,nCustomers,pHome,pOffice)<<endl;	    
 	}
 	
 	return 0;
