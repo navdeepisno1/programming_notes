69d4d1f478a3dd2988a6adfcf863c18dc79feb72
Shubham <99467100+TheGreatShubham@users.noreply.github.com>
2022-07-15 15:20:29 +0530

    Update Balloon Burst.cpp

--- samsung/SWC/algorithm/Balloon Burst.cpp
+++ samsung/SWC/algorithm/Balloon Burst.cpp
@@ -9,28 +9,34 @@ Algorithm
 */
 
 //CODE
-class Solution {
-public:
-    vector<vector<int>>dp;
-    int google(vector<int>& nums,int left,int right){
+      #include<bits/stdc++.h>
+	using namespace std;
+
+int dp[102][102];
+    int google(int *arr,int left,int right){
         if(left>right)return 0;
         if(dp[left][right]!=-1)return dp[left][right];
         int ans=INT_MIN;
         for(int lb=left;lb<=right;lb++){
-            int tempans=nums[left-1]*nums[right+1]*nums[lb];
-            int x = google(nums,left,lb-1);
-            int y = google(nums,lb+1,right);
+            int tempans=arr[left-1]*arr[right+1]*arr[lb];
+            int x = google(arr,left,lb-1);
+            int y = google(arr,lb+1,right);
             tempans+=(x+y);
             ans=max(ans,tempans);
         }
          return dp[left][right]=ans;
     }
-    int maxCoins(vector<int>& nums) {
-        int n=nums.size();
-        nums.insert(nums.begin(),1);
-        nums.push_back(1);
-        dp=vector<vector<int>>(n+2,vector<int>(n+2,-1));
-        
-        return google(nums,1,n);
+    int maxCoins(int *arr,int n) {
+        return google(arr,1,n);
     }
-};
+
+int main(){
+    int n;
+	cin>>n;
+	int arr[n+2];
+	arr[0]=arr[n+1]=1;
+	for(int i=1;i<=n;i++){
+		cin>>arr[i];
+	}
+	cout<<maxCoins(arr,n);
+}
