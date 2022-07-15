58078bcf66dbd959b0138f9e10322796c41bf57d
Shubham <99467100+TheGreatShubham@users.noreply.github.com>
2022-07-15 17:48:00 +0530

    Update Balloon Burst.cpp

--- samsung/SWC/algorithm/Balloon Burst.cpp
+++ samsung/SWC/algorithm/Balloon Burst.cpp
@@ -9,57 +9,114 @@ Algorithm
 */
 
 //CODE
+
+
+// Include header file 
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// Initializing  globally an 2D DP array
 int dp[102][102];
+
+
+// Our Utility function to calculate maximum coins after brusting
 int maxCoinsUtility(int *arr, int left, int right)
 {
+    // This is a recursion breaking condition
     if (left > right)
         return 0;
+	
+     // If we have already calculated for the left and right index
+    //  Then We will simply return our result.
     if (dp[left][right] != -1)
         return dp[left][right];
+	
+    // As we know that we want the maximum value that why we assign the minimum value to the ans variable.
     int ans = INT_MIN;
+	
+	
+    // Lb is the index of the last balloon burst in (left, right)
+  //, and to 
+  
     for (int lb = left; lb <= right; lb++)
-    {
+    {                          
+	                        // get the value of last balloon at index lb
 				int currentBalloon = arr[lb];        
 				
+	                         // the coins that burst will get are arr[left] * arr[lb] * arr[right]
 				int tempAns = arr[left - 1] * arr[right + 1] * currentBalloon;
+	    
+	 // we also need to add the coins obtained from bursting balloons between left and lb, and between lb and right
+	//i.e., dp[left][lb] and dp[lb][right]
         
+	    			// Calculating DP[left][lb]
 				int leftCoinsExcludingCurrentBalloon = google(arr, left, lb - 1);
-        int rightCoinsExcludingCurrentBalloon = google(arr, lb + 1, right);
-			
+	    
+	                        // Calculating DP[lb][right]
+                                int rightCoinsExcludingCurrentBalloon = google(arr, lb + 1, right);
+	    
+	// calculate dp[left][right]		
         tempAns += (leftCoinsExcludingCurrentBalloon + rightCoinsExcludingCurrentBalloon );
-			
+	
+	// Taking the maximum value between new and previous one.    
         ans = max(ans, tempAns);
     }
+	
+     // Basically Dp[left][right] represent the the maximum coins
+    // we get after  burst all the balloons between left and right in the original array.
     return dp[left][right] = ans;
 }
+
+
+// This is our helper function
 int getMaxCoins(int *arr, int n)
 {
+    // Call our Utility function 
     return maxCoinsUtility(arr, 1, n);
 }
 
+
+// Main function starts from here 
 int main()
 {
+    // t is the number of test cases	
     int t;
     cin >> t;
-
+	
+    //Here, we are solving the testcase one by one
     while (t--)
     {
+	// Taking input  of total number of balloons     
         int nBalloons;
         cin >> nBalloons;
+	    
+	// Initializing an array 
         int balloonCoins[nBalloons + 2];
+	    
+	    
+	 // Adding 1 in the fist and last position of the array because for the first position and last position
+	// Do not have first-1 and last+1 respectively.
         balloonCoins[0] = balloonCoins[nBalloons + 1] = 1;
+	       
+	    
+	// Taking value of Coins of balloons which will get after brust  
         for (int i = 1; i <= nBalloons; i++)
         {
             cin >> balloonCoins[i];
         }
+	    
+	    
+	// Putting all values in DP array is -1    
         for(int i=0;i<102;i++){
 		for(int j=0;j<102;j++){
 			dp[i][j]=-1;
 		}
 	}
+	    
+	    
+	 // Call our helper function "getMaxcoins" to Calculate the 
+	//  maximum coins we can collect by bursting the balloons wisely
         cout << getMaxCoins(balloonCoins, nCoins)<<endl;
     }
 }
