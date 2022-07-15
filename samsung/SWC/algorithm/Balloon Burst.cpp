ffc245b6163239abcc7a206899f6ccd8a8f79c2f
Navdeep Sharma <navdeepisno1@gmail.com>
2022-07-15 15:23:42 +0530

    added testcases

--- samsung/SWC/algorithm/Balloon Burst.cpp
+++ samsung/SWC/algorithm/Balloon Burst.cpp
@@ -9,34 +9,47 @@ Algorithm
 */
 
 //CODE
-      #include<bits/stdc++.h>
-	using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
 int dp[102][102];
-    int google(int *arr,int left,int right){
-        if(left>right)return 0;
-        if(dp[left][right]!=-1)return dp[left][right];
-        int ans=INT_MIN;
-        for(int lb=left;lb<=right;lb++){
-            int tempans=arr[left-1]*arr[right+1]*arr[lb];
-            int x = google(arr,left,lb-1);
-            int y = google(arr,lb+1,right);
-            tempans+=(x+y);
-            ans=max(ans,tempans);
-        }
-         return dp[left][right]=ans;
+int google(int *arr, int left, int right)
+{
+    if (left > right)
+        return 0;
+    if (dp[left][right] != -1)
+        return dp[left][right];
+    int ans = INT_MIN;
+    for (int lb = left; lb <= right; lb++)
+    {
+        int tempans = arr[left - 1] * arr[right + 1] * arr[lb];
+        int x = google(arr, left, lb - 1);
+        int y = google(arr, lb + 1, right);
+        tempans += (x + y);
+        ans = max(ans, tempans);
     }
-    int maxCoins(int *arr,int n) {
-        return google(arr,1,n);
-    }
+    return dp[left][right] = ans;
+}
+int maxCoins(int *arr, int n)
+{
+    return google(arr, 1, n);
+}
 
-int main(){
-    int n;
-	cin>>n;
-	int arr[n+2];
-	arr[0]=arr[n+1]=1;
-	for(int i=1;i<=n;i++){
-		cin>>arr[i];
-	}
-	cout<<maxCoins(arr,n);
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n + 2];
+        arr[0] = arr[n + 1] = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> arr[i];
+        }
+        cout << maxCoins(arr, n);
+    }
 }
--- samsung/SWC/algorithm/Cycle Detection In Directed Graph.cpp
+++ samsung/SWC/algorithm/Cycle Detection In Directed Graph.cpp
@@ -4,7 +4,66 @@
 /*
 Algorithm
 
+Note: As we cannot use STL ,we will create graph using Adjacency Matrix:
+      A[i][j] = 0  => No Edge from Node/Vertex i to Node/Vertex j
+      A[i][j] = 1  => Edge from Node/Vertex i to Node/Vertex j
+
+In order to prevent any error during Matrix pass in function we will use global variables like
+Adjacency Matrix, Visited Array,Recursion Stack Array       
+
+Basic Approach : We will do a DFS on the graph and check for cycle from every node. If we found a cycle print it.
+To detect the cycle we will use a recursion stack in the form of boolean array which will check for the element if present in
 
 */
 
 //CODE Starts here
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Graph size
+int V;
+
+//Global Arrays
+int graph[100][100];
+bool visited[100];
+bool recStack[100];
+
+void initializeGraph()
+{
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            graph[i][j] = 0;
+        }
+    }
+}
+
+void setVisitedState(bool state)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        visited[i] = false;
+    }
+}
+
+void setRecStackState(bool state)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        recStack[i] = false;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> V;
+    }
+}
