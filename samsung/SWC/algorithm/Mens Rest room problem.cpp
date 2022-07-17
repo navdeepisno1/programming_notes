49e8548c82fec9e8101955e07a8f7786460acb7a
Navdeep Sharma <navdeepisno1@gmail.com>
2022-07-17 21:44:42 +0530

    Added Mens Urinal Problem

--- samsung/SWC/algorithm/Mens Rest room problem.cpp
+++ samsung/SWC/algorithm/Mens Rest room problem.cpp
@@ -11,67 +11,100 @@ Basic approach : We will calculate the longest path of continuos empty urinals a
 Steps:
 1 -> Iterate for every men to occupy urinal
 2 -> Create three variables to track maximum continuos path length , current continuos path length, and postion where continuos path
-3 -> Iterate for every urinal postion and track maximum and 
-
+3 -> Iterate for every urinal postion and track maximum length continuos unoccupied urinals and and the location where it ends
+4 -> Now get the starting point of maximum length continuos unoccupied urinals by subtracting the last location by length - 1;
+5 -> Now place the men in the middle of the starting and ending point
 */
 
 //Code
 
+//Include necessary header files for standard input and output
 #include <iostream>
 
+//Namespace to avoid variable collision
 using namespace std;
 
-void solve(int urinalSlots[], int nUrinals, int nMens)
+//Main utility function
+void placeKMensInNUrinalsUtil(int urinalSlots[], int nUrinals, int nMens)
 {
+    //Iterate over every men
     for (int men = 0; men < nMens; men++)
     {
-        int maxContinuosEmptyUrinals = 0;
-        int currentContinuosEmptyUrinals = 0;
-        int lastPosmaxContinuosEmptyUrinals = 0;
+        //Tracking variables with self explanatory names
+        int maxContinuosEmptyUrinalsLength = 0;
+        int currentContinuosEmptyUrinalsLength = 0;
+        int lastPosmaxContinuosEmptyUrinalsLength = 0;
 
+        //Iterate over every position in Urinals and get maximum length continuos unoccupied urinals and its last location in array
         for (int pos = 0; pos < nUrinals; pos++)
         {
+            //For empty urinal increase length
             if (urinalSlots[pos] == 0)
             {
-                currentContinuosEmptyUrinals++;
+                currentContinuosEmptyUrinalsLength++;
             }
+            //Update the tracking variables
+            if (currentContinuosEmptyUrinalsLength >= maxContinuosEmptyUrinalsLength)
+            {
+                maxContinuosEmptyUrinalsLength = currentContinuosEmptyUrinalsLength;
+                lastPosmaxContinuosEmptyUrinalsLength = pos;
+            }
+
+            //Reset the length when we found occupied urinal
             if (urinalSlots[pos] == 1)
             {
-                currentContinuosEmptyUrinals = 0;
-            }
-            if (currentContinuosEmptyUrinals >= maxContinuosEmptyUrinals)
-            {
-                maxContinuosEmptyUrinals = currentContinuosEmptyUrinals;
-                lastPosmaxContinuosEmptyUrinals = pos;
+                currentContinuosEmptyUrinalsLength = 0;
             }
         }
 
-        int startPosInTheLongestContinuosPath = lastPosmaxContinuosEmptyUrinals - maxContinuosEmptyUrinals + 1;
-        int midPosInTheLongestContinuosPath = (startPosInTheLongestContinuosPath + lastPosmaxContinuosEmptyUrinals) / 2;
-        urinalSlots[midPosInTheLongestContinuosPath] = 1;
+        //Get starting postion and middle postion
+        int startPosInTheLongestContinuosPathLength = lastPosmaxContinuosEmptyUrinalsLength - maxContinuosEmptyUrinalsLength + 1;
+        int midPosInTheLongestContinuosPathLength = (startPosInTheLongestContinuosPathLength + lastPosmaxContinuosEmptyUrinalsLength) / 2;
+
+        //Set/Occupy the Urinal
+        urinalSlots[midPosInTheLongestContinuosPathLength] = 1;
     }
 }
 
+//Main Function
 int main()
 {
+    //Get number of test cases
     int t;
     cin >> t;
 
+    //Run the loop until t becomes 0 (0==false)
     while (t--)
     {
+        //Get number of Urinals
         int nUrinals;
         cin >> nUrinals;
 
+        //Get number of Mens
         int nMens;
         cin >> nMens;
 
+        //Create binary array of Urinal slots of size nUrinals
         int urinalSlots[nUrinals];
 
+        //Make every Urinal Unoccupied in starting (0 = Unoccupied  || 1 = Occupied)
         for (int i = 0; i < nUrinals; i++)
         {
             urinalSlots[i] = 0;
         }
 
-        solve(urinalSlots, nUrinals, nMens);
+        //Call the function
+        placeKMensInNUrinalsUtil(urinalSlots, nUrinals, nMens);
+
+        //Print the Urinals
+        for (int i = 0; i < nUrinals; i++)
+        {
+            cout << urinalSlots[i] << "";
+        }
+
+        cout << endl;
     }
+
+    //Yeah program executed successfully
+    return 0;
 }
