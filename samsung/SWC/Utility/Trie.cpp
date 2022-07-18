a894cd0ff87166f093677a56ab4f19dd924ac67d
Navdeep Sharma <navdeepisno1@gmail.com>
2022-07-18 22:52:31 +0530

    Added Trie

---
+++ samsung/SWC/Utility/Trie.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define SIZE 26;
+
+using namespace std;
+
+struct TrieNode
+{
+    struct TrieNode *children[26];
+    bool isEndOfWord;
+};
+
+struct TrieNode *getNode()
+{
+}
+
+void insert(struct TrieNode *root, string key)
+{
+}
+
+bool search(struct TrieNode *root, string key)
+{
+}
